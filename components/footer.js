a077b78e206827c1484b8baf1e4ba037f88be3c0
vlachvojta <vojtik.vlach@seznam.cz>
2026-01-24 18:39:47 +0100

    Update footer link and formulation

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <footer className="w-full flex flex-col items-center mt-4">
       <div className="flex flex-col-reverse md:flex-row justify-between flex-wrap items-center w-[90vw]">
-        <p className="font-light mb-6">
+        <span className="font-light mb-0">
           © {year},{" "}
           <Link
             href="https://www.bihk.cz/biskupstvi/diecezni-centrum-pro-mladez"
@@ -21,14 +21,15 @@ export default function Footer() {
           >
             DCM Hradec Králové
           </Link>
-        </p>
+        </span>
         <div className="my-4 text-xs uppercase">
+          <p className="text-center">Zavzpomínej na minulé ročníky:</p>
           <Link
             href="https://2023.diecezko.cz/"
             target="_blank"
             className="cursor-pointer underline"
           >
-            Diecézko podzim 2023
+            Podzim 2023
           </Link>
           {" • "}
           <Link
@@ -36,15 +37,15 @@ export default function Footer() {
             target="_blank"
             className="cursor-pointer underline"
           >
-            Diecézko jaro 2024
+            Jaro 2024
           </Link>
             {" • "}
           <Link
-            href="https://2025.diecezko.cz/"
+            href="/galerie"
             target="_blank"
             className="cursor-pointer underline"
           >
-            Diecézko jaro 2025
+            Jaro 2025
           </Link>
         </div>
         <div className="flex flex-row items-center justify-around min-w-[150px]">
