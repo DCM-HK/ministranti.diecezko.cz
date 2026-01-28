87b9b0c18584cb1325d4b23e35ab4f3b353eea97
vlachvojta <vojtik.vlach@seznam.cz>
2026-01-28 14:06:28 +0100

    add responsive buildings in the background

--- components/cover.js
+++ components/cover.js
@@ -11,24 +11,60 @@ export default function Cover({ children }) {
   };
 
   useLayoutEffect(() => {
+    handleScroll();
     window.addEventListener("scroll", handleScroll);
-  });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <div className="fixed top-0 h-screen w-full -z-10 flex flex-col">
+      <div className="fixed inset-0 -z-10">
         <div
-          className={`absolute h-screen w-full transition-all z-10 duration-[2000ms] ${blur}`}
+          className={`absolute inset-0 transition-all z-20 duration-[2000ms] ${blur}`}
         ></div>
-        <picture>
+        <picture className="absolute inset-0">
           <source srcSet="assets/images/cover.webp" type="image/webp" />
           <source srcSet="assets/images/cover.jpg" type="image/jpeg" />
           <img
-            alt="Podium se zataženým závěsem"
+            alt="Obloha s mraky a barvami zapadajícího slunce"
             loading="lazy"
             src="assets/images/cover.jpg"
-            className="h-screen w-full -z-20 object-cover object-bottom"
+            className="h-screen w-full object-cover object-bottom"
           />
         </picture>
+        <div className="pointer-events-none hidden lg:block">
+          <picture className="absolute bottom-0 left-0 w-[clamp(100px,20vw,360px)]">
+            <source
+              srcSet="assets/images/budova-katedrala.webp"
+              type="image/webp"
+            />
+            <source
+              srcSet="assets/images/budova-katedrala.png"
+              type="image/png"
+            />
+            <img
+              alt="Katedrála"
+              loading="lazy"
+              src="assets/images/budova-katedrala.png"
+              className="w-full h-auto object-contain"
+            />
+          </picture>
+          <picture className="absolute bottom-0 right-0 w-[clamp(100px,20vw,360px)]">
+            <source
+              srcSet="assets/images/budova-bila-vez.webp"
+              type="image/webp"
+            />
+            <source
+              srcSet="assets/images/budova-bila-vez.png"
+              type="image/png"
+            />
+            <img
+              alt="Bílá věž"
+              loading="lazy"
+              src="assets/images/budova-bila-vez.png"
+              className="w-full h-auto object-contain"
+            />
+          </picture>
+        </div>
       </div>
       <div className="flex items-center flex-col w-full">{children}</div>
     </>
