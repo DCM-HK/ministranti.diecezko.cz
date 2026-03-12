5a8e5e9e0feeea85dd96774de5015a7794acc2f8
vlachvojta <vojtik.vlach@seznam.cz>
2026-03-12 01:15:54 +0100

    add preddiecezka and carpool slash trainpool

--- app/preddiecezka/page.js
+++ app/preddiecezka/page.js
@@ -28,6 +28,26 @@ export default function GroupTransport() {
             Některé vikariáty pořádají menší setkání (tzv. vikariátko) den před Diecézkem a&nbsp;poté společně vyrážejí do Hradce Králové. Tomuto setkání říkáme PředDiecézko. Zde nalezneš přehled, který tě nasměruje na další informace.
           </ContainerDescription>
           <FaqLayout>
+            <FaqBox question={`Hradec Králové`} icon={``}>
+              <ContainerParagraph>
+                V pátek 27. 3. se v našem vikariátu uskuteční před-diecézní setkání mládeže jako
+                příležitost společně se naladit na blížící se diecézní setkání. Začne v 15:30 ve
+                farní místnosti v Novém Adalbertinu. Program nabídne hry, krátké témátko, modlitbu
+                i prostor pro setkání a sdílení. Na závěr se zapojíme do přípravy křížové cesty na
+                Novém Hradci Králové, kam se společně přesuneme MHD.
+              </ContainerParagraph>
+              <ContainerParagraph>
+                Více informací na instagramu:&nbsp;
+                <Link
+                  href="https://www.instagram.com/spolco_hk/"
+                  target="_blank"
+                  rel="external noopener nofollow"
+                  className="underline"
+                >
+                  @spolco_hk
+                </Link>.
+              </ContainerParagraph>
+            </FaqBox>
             <FaqBox question={`Humpolec`} icon={``}>
               <ContainerImageBase
                 jpgPath="/assets/images/preddiecezka/preddiecezko-hum.jpg"
@@ -63,6 +83,23 @@ export default function GroupTransport() {
                 altText="Plakátek s informacemi o PředDiecézku vikariátu Ústí nad Orlicí."
                 className="w-full my-4"
               />
+            </FaqBox>
+            <FaqBox question={`Žamberk`} icon={``}>
+              <ContainerParagraph>
+                Společný odjezd pro zájemce z žambereckého vikariátu bude v sobotu 28.3. v 7:33
+                vlakem z Letohradu. Těšíme se na vikariátním setkání nebo na Diecézku!&nbsp;
+                <Link
+                  href="https://docs.google.com/forms/d/e/1FAIpQLScsjUTi_KN9X9Z-399Ads99IY7ERx5GRcARQFWDkZGGOoA1hA/viewform?usp=send_form"
+                  target="_blank" rel="external noopener nofollow" className="underline">
+                  Přihláška na předdiecézko
+                </Link>.
+              </ContainerParagraph>
+              <ContainerImageBase
+                jpgPath="/assets/images/spolecna-doprava/zamberk.jpg"
+                webpPath="/assets/images/spolecna-doprava/zamberk.webp"
+                altText="Plakátek s informacemi o společné dopravě."
+                className="w-full my-4"
+                />
             </FaqBox>
             {/*
             <FaqBox question={`Jičín`} icon={``}>
