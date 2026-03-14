7b098229edb82d55c6121118899f86632444f14d
vlachvojta <vojtik.vlach@seznam.cz>
2026-03-14 18:13:50 +0100

    allow text alignment to overwrite default text-justify

--- components/layouts/container-paragraph.js
+++ components/layouts/container-paragraph.js
@@ -1,3 +1,8 @@
-export default function ContainerParagraph({ className, children }) {
-  return <p className={`${className} my-2 tracking-normal text-lg w-full text-justify`}>{children}</p>;
+import { hasTextAlignmentClass } from "../../lib/text-alignment";
+
+export default function ContainerParagraph({ className = "", children }) {
+  const hasTextAlignment = hasTextAlignmentClass(className);
+  const paragraphClassName = `my-2 w-full text-lg tracking-normal ${hasTextAlignment ? "" : "text-justify"} ${className}`.trim();
+
+  return <p className={paragraphClassName}>{children}</p>;
 }
