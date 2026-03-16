9a6b3e2626d3f44e616ffcbfd7abcff20fb90f77
Vojtěch Vlach <vojtechvlach.vv@gmail.com>
2026-03-16 09:14:53 +0100

    remove the nbsp alltogether

--- app/sekce-pro-mladez/page.js
+++ app/sekce-pro-mladez/page.js
@@ -27,7 +27,7 @@ export default async function Pehuma() {
             <Link href="https://cbk.cirkev.cz" target="_blank" rel="external noopener nofollow" className="underline">
               České biskupské konference
             </Link>
-            &nbsp;koordinuje pastoraci mládeže v celé České republice. Vznikla v roce 1990,
+            koordinuje pastoraci mládeže v celé České republice. Vznikla v roce 1990,
             aby propojovala práci s mladými lidmi v jednotlivých diecézích a od začátku podporuje
             jejich zapojení do života církve. 
           </ContainerDescription>
