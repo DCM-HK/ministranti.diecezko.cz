f53a4438f1488b40be5ff12a81c1cd7785d260d1
vlachvojta <vojtik.vlach@seznam.cz>
2026-05-01 13:15:13 +0200

    add nbsps

--- app/jadro/page.js
+++ app/jadro/page.js
@@ -39,11 +39,12 @@ export default async function Vikariaty() {
     </main>
   );
 }
+
 export const jadro = [
   {
     name: "o. b. Prokop Brož",
     description:
-      "Ministrovat začal dříve, než šel do školy. Poté co byl vysvěcen na jáhna (1996), na kněze (1997) a potom na biskupa (2025), je rád, když během liturgie zažije u oltáře výbornou spolupráci s ministranty. Docela dlouhou dobu se věnoval učení na školách základních, středních i vysokých. A tak když spojuje svůj osobní život s modlitbou a s liturgickým slavením eucharistie, stále se snaží učit novým věcem. Třeba i pracovat s ministranty v královéhradecké diecézi.",
+      "Ministrovat začal dříve, než šel do školy. Poté co byl vysvěcen na jáhna (1996), na kněze (1997) a&nbsp;potom na biskupa (2025), je rád, když během liturgie zažije u&nbsp;oltáře výbornou spolupráci s&nbsp;ministranty. Docela dlouhou dobu se věnoval učení na školách základních, středních i&nbsp;vysokých. A&nbsp;tak když spojuje svůj osobní život s&nbsp;modlitbou a&nbsp;s&nbsp;liturgickým slavením eucharistie, stále se snaží učit novým věcem. Třeba i&nbsp;pracovat s&nbsp;ministranty v&nbsp;královéhradecké diecézi.",
     jpgPath: "/assets/images/jadro/broz.jpg",
     webpPath: "/assets/images/jadro/broz.webp",
     altText: "Profilová fotka o. biskupa Prokopa Brože",
@@ -52,7 +53,7 @@ export const jadro = [
   {
     name: "P. Petr Soukal",
     description:
-      "Je knězem od roku 2017. Jako kaplan působil v Havlíčkově Brodě a nyní je farářem v Ústí nad Orlicí. Jako kluk rád ministroval, jezdil na kole a hrál na kytaru. Zajímá se o teologii, rád si povídá o Pánu Bohu a těší ho potkávat lidi, kteří přemýšlejí a kladou si otázky.",
+      "Je knězem od roku 2017. Jako kaplan působil v&nbsp;Havlíčkově Brodě a&nbsp;nyní je farářem v&nbsp;Ústí nad Orlicí. Jako kluk rád ministroval, jezdil na kole a&nbsp;hrál na kytaru. Zajímá se o&nbsp;teologii, rád si povídá o&nbsp;Pánu Bohu a&nbsp;těší ho potkávat lidi, kteří přemýšlejí a&nbsp;kladou si otázky.",
     jpgPath: "/assets/images/jadro/soukal.jpg",
     webpPath: "/assets/images/jadro/soukal.webp",
     altText: "Profilová fotka P. Petra Soukala",
@@ -61,7 +62,7 @@ export const jadro = [
   {
     name: "P. Jenda Pitřinec",
     description:
-      "Knězem je stejně dlouho jako Petr S. a Vojtěch N., tedy od května 2017. Působil ve farnostech Litomyšl a Ústí nad Orlicí a nyní má na starosti Vesmír - centrum pro mládež v Deštném v Orlických horách. Jako malý zlobil v kostele tak, že ho uklidnila jen služba u oltáře. Má rád sport v jakékoli podobě a spoustu dalších věcí.",
+      "Knězem je stejně dlouho jako Petr S. a&nbsp;Vojtěch N., tedy od května 2017. Působil ve farnostech Litomyšl a&nbsp;Ústí nad Orlicí a&nbsp;nyní má na starosti Vesmír - centrum pro mládež v&nbsp;Deštném v&nbsp;Orlických horách. Jako malý zlobil v&nbsp;kostele tak, že ho uklidnila jen služba u&nbsp;oltáře. Má rád sport v&nbsp;jakékoli podobě a&nbsp;spoustu dalších věcí.",
     jpgPath: "/assets/images/jadro/pitrinec.png",
     webpPath: "/assets/images/jadro/pitrinec.webp",
     altText: "Profilová fotka P. Jendy Pitřince",
@@ -70,7 +71,7 @@ export const jadro = [
   {
     name: "P. Vojtěch Novotný",
     description:
-      "Hádejte, jak dlouho je knězem. Nápovědu hledejte výše. V současné době je farářem u katedrály v Hradci Králové. Ke kněžství nebyl povolán od rybářských sítí, ale ze světa čísel. Od dětství totiž miluje matematiku. Čtvrtinu svého života prožil v Římě. Do školy chodí už 32 let.",
+      "Hádejte, jak dlouho je knězem. Nápovědu hledejte výše. V&nbsp;současné době je farářem u&nbsp;katedrály v&nbsp;Hradci Králové. Ke kněžství nebyl povolán od rybářských sítí, ale ze světa čísel. Od dětství totiž miluje matematiku. Čtvrtinu svého života prožil v&nbsp;Římě. Do školy chodí už 32 let.",
     jpgPath: "/assets/images/jadro/novotny.jpg",
     webpPath: "/assets/images/jadro/novotny.webp",
     altText: "Profilová fotka P. Vojtěcha Novotného",
@@ -79,7 +80,7 @@ export const jadro = [
   {
     name: "Matyáš Mesi Meisner",
     description:
-      "Ceremonář pomocného biskupa a koordinátor ministrantů v diecézi. Služba u oltáře je mu blízká už dlouho, a proto má radost, že se může podílet na setkání, které spojuje ministranty z celé diecéze. Zároveň aktivně hraje basketbal.",
+      "Ceremonář pomocného biskupa a&nbsp;koordinátor ministrantů v&nbsp;diecézi. Služba u&nbsp;oltáře je mu blízká už dlouho, a&nbsp;proto má radost, že se může podílet na setkání, které spojuje ministranty z&nbsp;celé diecéze. Zároveň aktivně hraje basketbal.",
     jpgPath: "/assets/images/jadro/meisner.png",
     webpPath: "/assets/images/jadro/meisner.webp",
     altText: "Profilová fotka Matyáše Meisnera",
