8fcafd95892a5eb2361b771852b83622b73532bf
vlachvojta <vojtik.vlach@seznam.cz>
2026-05-01 12:59:28 +0200

    update jadro people + texts

--- app/jadro-diecezka/page.js
+++ app/jadro-diecezka/page.js
@@ -13,17 +13,12 @@ export default async function Vikariaty() {
       <Header />
       <PageMain>
         <Container>
-          <ContainerTitle>Jádro Diecézka (Éčko)</ContainerTitle>
+          <ContainerTitle>Jádro setkání ministrantů</ContainerTitle>
           <ContainerDescription>
-            Diecézní setkání mládeže je možné zorganizovat díky dobrovolníkům,
-            kteří se&nbsp;do jeho příprav zapojili. Tým, který se&nbsp;schází
-            již od&nbsp;podzima, aby pro vás Diecézko připravil, si&nbsp;říká
-            Éčko. Víte proč? Podle maskota mládeže královéhradecké diecéze
-            holuba Evžena! Se&nbsp;členy Éčka a&nbsp;jejich úkoly se&nbsp;můžete
-            seznámit níže.
+            TODO - nějakej textík tady?? Jak se sešel takovýhle tým? Triumvirát lore?? <br />
           </ContainerDescription>
           <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 grid-flow-row gap-4 items-center">
-            {ecko.map((p) => {
+            {jadro.map((p) => {
               return (
                 <ProfileCard
                   key={p.name}
@@ -44,139 +39,50 @@ export default async function Vikariaty() {
     </main>
   );
 }
-export const ecko = [
+export const jadro = [
   {
-    name: "Evžen Holub",
-    description: "Pan Holub drží holubí dohled nad celým Diecézkem.",
-    jpgPath: "/assets/images/jadro/evzen.jpg",
-    webpPath: "/assets/images/jadro/evzen.webp",
-    altText: "Profilová fotka Holuba Evžena",
+    name: "o. b. Prokop Brož",
+    description:
+      "Ministrovat začal dříve, než šel do školy. Poté co byl vysvěcen na jáhna (1996), na kněze (1997) a potom na biskupa (2025), je rád, když během liturgie zažije u oltáře výbornou spolupráci s ministranty. Docela dlouhou dobu se věnoval učení na školách základních, středních i vysokých. A tak když spojuje svůj osobní život s modlitbou a s liturgickým slavením eucharistie, stále se snaží učit novým věcem. Třeba i pracovat s ministranty v královéhradecké diecézi.",
+    jpgPath: "/assets/images/jadro/broz.jpg",
+    webpPath: "/assets/images/jadro/broz.webp",
+    altText: "Profilová fotka o. biskupa Prokopa Brože",
+    className: "",
+  },
+  {
+    name: "P. Petr Soukal",
+    description:
+      "Je knězem od roku 2017. Jako kaplan působil v Havlíčkově Brodě a nyní je farářem v Ústí nad Orlicí. Jako kluk rád ministroval, jezdil na kole a hrál na kytaru. Zajímá se o teologii, rád si povídá o Pánu Bohu a těší ho potkávat lidi, kteří přemýšlejí a kladou si otázky.",
+    jpgPath: "/assets/images/jadro/soukal.jpg",
+    webpPath: "/assets/images/jadro/soukal.webp",
+    altText: "Profilová fotka P. Petra Soukala",
     className: "",
   },
   {
     name: "P. Jenda Pitřinec",
     description:
-      "Jenda je nejen ředitelem Vesmíru a&nbsp;DCM, ale hlavně duchovní opora celého přípravného týmu. Rád se zamýšlí nad průběhem celého Diecézka a&nbsp;přichází s&nbsp;pointy k&nbsp;vylepšení.",
+      "Knězem je stejně dlouho jako Petr S. a Vojtěch N., tedy od května 2017. Působil ve farnostech Litomyšl a Ústí nad Orlicí a nyní má na starosti Vesmír - centrum pro mládež v Deštném v Orlických horách. Jako malý zlobil v kostele tak, že ho uklidnila jen služba u oltáře. Má rád sport v jakékoli podobě a spoustu dalších věcí.",
     jpgPath: "/assets/images/jadro/pitrinec.jpg",
     webpPath: "/assets/images/jadro/pitrinec.webp",
-    altText: "Profilová fotka o. Jendy Pitřince",
+    altText: "Profilová fotka P. Jendy Pitřince",
     className: "",
   },
   {
-    name: "Mons. Prokop Brož",
+    name: "P. Vojtěch Novotný",
     description:
-      "Biskup Prokop má na starosti pastorační činnost v&nbsp;naší diecézi. Stará se o&nbsp;to, aby všichni táhli za jeden provaz. Řídí se heslem: Bez Ducha Svatého ani ránu!",
-    jpgPath: "/assets/images/jadro/broz.jpg",
-    webpPath: "/assets/images/jadro/broz.webp",
-    altText: "Profilová fotka Mons. Prokopa Brože",
-    className: "",
-  },  
-  {
-    name: "Klárka Petrová",
-    description:
-      "Klárka je vedoucí DCM. Koordinuje přípravu a&nbsp;průběh Diecézka. Též se stará o&nbsp;všechny, kteří setkání připravují.",
-    jpgPath: "/assets/images/jadro/petrova.jpg",
-    webpPath: "/assets/images/jadro/petrova.webp",
-    altText: "Profilová fotka Klárky Petrové",
-    className: "",
-  },
-  {
-    name: "Klárka Kopecká",
-    description:
-      "Manažerka Klárka komunikuje s&nbsp;hosty. Domlouvá s&nbsp;nimi detaily jejich přednášek a&nbsp;stará se o&nbsp;ně v&nbsp;průběhu Diecézka.",
-    jpgPath: "/assets/images/jadro/kopecka.jpg",
-    webpPath: "/assets/images/jadro/kopecka.webp",
-    altText: "Profilová fotka Klárky Kopecké",
-    className: "",
-  },
-  {
-    name: "Josi Pazderová",
-    description:
-      "Josi vás se svým týmem označí náramkem při vstupu do Filharmonie. Také se stará o&nbsp;to, aby po Diecézku bylo všude uklizeno.",
-    jpgPath: "/assets/images/jadro/pazderova.jpg",
-    webpPath: "/assets/images/jadro/pazderova.webp",
-    altText: "Profilová fotka Josi Pazderové",
-    className: "",
-  },
-  {
-    name: "Anička Marie Maříková",
-    description:
-      "Anička má na starosti režijní program, který nás bude provázet během dopoledne ve Filharmonii.",
-    jpgPath: "/assets/images/jadro/marikova.jpg",
-    webpPath: "/assets/images/jadro/marikova.webp",
-    altText: "Profilová fotka Aničky Maříkové",
-    className: "",
-  },
-  {
-    name: "Terezka Škarydová",
-    description:
-      "Terezka společně s&nbsp;Aničkou&nbsp;K. pečují o&nbsp;pohodlí našich žaludků po celý den.",
-    jpgPath: "/assets/images/jadro/skarydova.jpg",
-    webpPath: "/assets/images/jadro/skarydova.webp",
-    altText: "Profilová fotka Terezky Škarydové",
-    className: "",
-  },  
-  {
-    name: "Anička Koubková",
-    description:
-      "Anička společně s&nbsp;Terezkou pečují o&nbsp;pohodlí našich žaludků po celý den.",
-    jpgPath: "/assets/images/jadro/koubkova.jpg",
-    webpPath: "/assets/images/jadro/koubkova.webp",
-    altText: "Profilová fotka Aničky Koubkové",
-    className: "",
-  },
-  {
-    name: "Dan Blažek",
-    description:
-      "Dan a&nbsp;jeho tým v&nbsp;oranžových vestičkách vám zajistí bezpečný doprovod až na přednášku.",
-    jpgPath: "/assets/images/jadro/blazek.jpg",
-    webpPath: "/assets/images/jadro/blazek.webp",
-    altText: "Profilová fotka Dana Blažka",
+      "Hádejte, jak dlouho je knězem. Nápovědu hledejte výše. V současné době je farářem u katedrály v Hradci Králové. Ke kněžství nebyl povolán od rybářských sítí, ale ze světa čísel. Od dětství totiž miluje matematiku. Čtvrtinu svého života prožil v Římě. Do školy chodí už 32 let.",
+    jpgPath: "/assets/images/jadro/novotny.jpg",
+    webpPath: "/assets/images/jadro/novotny.webp",
+    altText: "Profilová fotka P. Vojtěcha Novotného",
     className: "",
   },
   {
     name: "Matyáš Mesi Meisner",
     description:
-      "Mesi připravuje slavnostní slavení společné mše svaté na závěr Diecézka. Též přispívá svými praktickými poznámkami k&nbsp;průběhu setkání.",
+      "Ceremonář pomocného biskupa a koordinátor ministrantů v diecézi. Služba u oltáře je mu blízká už dlouho, a proto má radost, že se může podílet na setkání, které spojuje ministranty z celé diecéze. Zároveň aktivně hraje basketbal.",
     jpgPath: "/assets/images/jadro/meisner.jpg",
     webpPath: "/assets/images/jadro/meisner.webp",
     altText: "Profilová fotka Matyáše Meisnera",
-    className: "",
-  },       
-  {
-    name: "Jindra Řezníček",
-    description:
-      "Jindra se stará o&nbsp;kaplany pro mládež a&nbsp;jejich zástupce a pomáhá sytit web obsahem.",
-    jpgPath: "/assets/images/jadro/reznicek.jpg",
-    webpPath: "/assets/images/jadro/reznicek.webp",
-    altText: "Profilová fotka Jindry Řezníčka",
-    className: "",
-  },
-  {
-    name: "Martin Rychtár",
-    description:
-      "Martin se svým týmem připravuje zázemí na BiGy pro celý přípravný tým a odpolední přednášky",
-    jpgPath: "/assets/images/vikariaty/rychtar.jpg",
-    webpPath: "/assets/images/vikariaty/rychtar.webp",
-    altText: "Profilová fotka Martina Rychtára",
-    className: "",
-  },
-  /* {
-    name: "Pavel Vocl",
-    description:
-      "Pavel má na starosti technickou stránku režijního programu ve Filharmonii.",
-    altText: "Profilová fotka Pavel Vocl",
-    jpgPath: "/assets/images/jadro/vocl.jpg",
-    webpPath: "/assets/images/jadro/vocl.webp",
-    className: "",
-  }, */
-  {
-    name: "Vojta Vlach",
-    description:
-      "Vojta vytváří web diecézko.cz a&nbsp;pomáhá s&nbsp;technickým zabezpečením celé akce.",
-    jpgPath: "/assets/images/jadro/vlach.jpg",
-    webpPath: "/assets/images/jadro/vlach.webp",
-    altText: "Profilová fotka Vojty Vlacha",
     className: "",
   },
 ];
