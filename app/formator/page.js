0c757b8b17f861704e032f147f63be2f65ff2524
vlachvojta <vojtik.vlach@seznam.cz>
2026-05-11 06:39:49 +0200

    add formator

--- app/formator/page.js
+++ app/formator/page.js
@@ -17,13 +17,13 @@ export default async function BistroUDvouPratel() {
             Formátor je člověk, který se věnuje formaci ministrantů.
           </ContainerDescription>
           <ContainerParagraph>
-            Formátor je osoba ustanovená v konkrétní oblasti. Ideální stav je mít v každé aktivní
-            oblasti alespoň jednoho. Za vhodného formátora je považován člověk, který se již aktivně
-            věnuje vedení ministrantského společenství. Formátorem může být i kněz, ale vzhledem
-            k současným podmínkám se jako praktičtější jeví, aby byla tato služba delegována na:
-            mladého člověka se zájmem o službu u oltáře a o rozvoj ministrantů v diecézi, nebo
-            rodiče, kteří se aktivně zapojuje do služeb ve farnosti a chce svým dětem dopřát
-            kvalitní formaci.
+            Formátor je osoba aktivně se podílející na pastoraci ministrantů v konkrétní oblasti.
+            Ideální stav je mít v každé aktivní oblasti alespoň jednoho. Za vhodného formátora je
+            považován člověk, který se již aktivně věnuje vedení ministrantského společenství.
+            Formátorem může být i kněz, ale vzhledem k současným podmínkám se jako praktičtější
+            jeví, aby byla tato služba delegována na mladého člověka se zájmem o službu u oltáře
+            a o rozvoj ministrantů v diecézi, nebo rodiče, kteří se aktivně zapojuje do služeb
+            ve farnosti a chce svým dětem dopřát kvalitní formaci.
           </ContainerParagraph>
         </Container>
       </PageMain>
